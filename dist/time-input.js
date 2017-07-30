674582e97214be4ebab4391dce4361839ef2786c
Noam Lin <noamlin@gmail.com>
2017-07-30 17:34:33 +0300

    only operate on the ngModel

--- dist/time-input.js
+++ dist/time-input.js
@@ -5,18 +5,9 @@ angular.module("time-input", []).directive("timeInput", [ function() {
     template: '<input type="number" class="hours" placeholder="HH" min="0" max="23">:<input type="number" class="minutes" placeholder="MM" min="0" max="59">:<input type="number" class="seconds" placeholder="SS" min="0" max="59">',
     restrict: "C",
     require: "ngModel",
-    controller: [ "$scope", function($scope) {
-      $scope.timeObject = {
-        hours: 0,
-        minutes: 0,
-        seconds: 0,
-        lastValidHours: 0,
-        lastValidMinutes: 0,
-        lastValidSeconds: 0
-      };
-    } ],
+    controller: [ "$scope", function($scope) {} ],
     link: function(scope, element, attrs, ngModel) {
-      var $inputs = element.find("input"), $hourInput = $inputs.eq(0), $minuteInput = $inputs.eq(1), $secondsInput = $inputs.eq(2), mousewheel = false, timeout;
+      var $inputs = element.find("input"), $hoursInput = $inputs.eq(0), $minutesInput = $inputs.eq(1), $secondsInput = $inputs.eq(2);
       function leadingZero(value) {
         if (value < 10) {
           return "0" + value;
@@ -24,95 +15,84 @@ angular.module("time-input", []).directive("timeInput", [ function() {
           return "" + value;
         }
       }
-      $("input", element).on("click", function() {
+      $inputs.on("click", function() {
         this.select();
       });
-      $("input", element).on("focus", function() {
+      $inputs.on("focus", function() {
         element.addClass("focused");
       });
-      $("input", element).on("mousewheel", function() {
-        mousewheel = true;
-        clearTimeout(timeout);
-        timeout = setTimeout(function() {
-          mousewheel = false;
-        }, 500);
-      });
-      $("input", element).bind("keydown", function(event) {
+      $inputs.on("mousewheel", function() {});
+      $inputs.on("keydown", function(event) {
         var key = event.keyCode;
         if (!(key >= 48 && key <= 57 || key >= 96 && key <= 105 || key === 8 || key === 46 || key >= 37 && key <= 40)) {
           event.preventDefault();
         }
       });
-      $("input", element).bind("paste cut", function(event) {
+      $inputs.on("paste cut", function(event) {
         event.preventDefault();
       });
       ngModel.$render = function render() {
         var date = ngModel.$viewValue;
-        scope.timeObject.hours = date.getHours();
-        scope.timeObject.minutes = date.getMinutes();
-        scope.timeObject.seconds = date.getSeconds();
-        scope.timeObject.lastValidHours = scope.timeObject.hours;
-        scope.timeObject.lastValidMinutes = scope.timeObject.minutes;
-        scope.timeObject.lastValidSeconds = scope.timeObject.seconds;
-        $hourInput.val(leadingZero(scope.timeObject.hours));
-        $minuteInput.val(leadingZero(scope.timeObject.minutes));
-        $secondsInput.val(leadingZero(scope.timeObject.seconds));
+        $hoursInput.val(leadingZero(date.getHours()));
+        $minutesInput.val(leadingZero(date.getMinutes()));
+        $secondsInput.val(leadingZero(date.getSeconds()));
       };
       scope.$watch(attrs.disable, function(newVal) {
         if (newVal) {
-          $hourInput.attr("disabled", "disabled");
-          $minuteInput.attr("disabled", "disabled");
+          $hoursInput.attr("disabled", "disabled");
+          $minutesInput.attr("disabled", "disabled");
           $secondsInput.attr("disabled", "disabled");
         } else {
-          $hourInput.removeAttr("disabled");
-          $minuteInput.removeAttr("disabled");
+          $hoursInput.removeAttr("disabled");
+          $minutesInput.removeAttr("disabled");
           $secondsInput.removeAttr("disabled");
         }
       });
       function shouldUpdateOnChange() {
-        return !(ngModel.$options && ngModel.$options.updateOn && ngModel.$options.updateOn.indexOf("default") === -1);
+        return ngModel.$options && ngModel.$options.updateOnChange === true;
       }
       function updateModel() {
-        var date = ngModel.$viewValue;
-        date.setSeconds(scope.timeObject.seconds);
-        date.setMinutes(scope.timeObject.minutes);
-        date.setHours(scope.timeObject.hours);
-        ngModel.$setViewValue(date);
+        ngModel.$viewValue.setHours(parseInt($hoursInput.val(), 10));
+        ngModel.$viewValue.setMinutes(parseInt($minutesInput.val(), 10));
+        ngModel.$viewValue.setSeconds(parseInt($secondsInput.val(), 10));
+        ngModel.$setViewValue(ngModel.$viewValue);
         ngModel.$commitViewValue();
       }
       function inputChange(elem, varName) {
         return function inputChangeClosure() {
           if (!elem.value || isNaN(elem.value)) {
-            scope.timeObject[varName] = 0;
-            scope.timeObject["lastValid" + varName] = 0;
             elem.value = "00";
             updateModel();
             $(elem).select();
             return;
           }
           if (elem.value.length > 2) {
-            elem.value = leadingZero(scope.timeObject["lastValid" + varName]);
+            switch (varName) {
+             case "hours":
+              elem.value = leadingZero(ngModel.$viewValue.getHours());
+              break;
+
+             case "minutes":
+              elem.value = leadingZero(ngModel.$viewValue.getMinutes());
+              break;
+
+             case "seconds":
+              elem.value = leadingZero(ngModel.$viewValue.getSeconds());
+              break;
+            }
             return;
           }
           if (Number(elem.value) > Number(elem.getAttribute("max"))) {
             elem.value = elem.value[0];
           }
-          scope.timeObject[varName] = Number(elem.value);
-          if ((elem.value.length === 2 || elem.value > Number(elem.getAttribute("max")[0])) && !mousewheel) {
-            elem.value = leadingZero(scope.timeObject[varName]);
-          }
-          if (mousewheel) {
-            elem.value = leadingZero(scope.timeObject[varName]);
-          }
-          scope.timeObject["lastValid" + varName] = scope.timeObject[varName];
           if (shouldUpdateOnChange()) {
             updateModel();
           }
         };
       }
-      $hourInput.bind("input", inputChange($hourInput.get(0), "hours"));
-      $minuteInput.bind("input", inputChange($minuteInput.get(0), "minutes"));
-      $secondsInput.bind("input", inputChange($secondsInput.get(0), "seconds"));
+      $hoursInput.on("input", inputChange($hoursInput.get(0), "hours"));
+      $minutesInput.on("input", inputChange($minutesInput.get(0), "minutes"));
+      $secondsInput.on("input", inputChange($secondsInput.get(0), "seconds"));
       function onKeyUp($nextInput) {
         return function(event) {
           var key = event.keyCode;
@@ -124,15 +104,15 @@ angular.module("time-input", []).directive("timeInput", [ function() {
           }
         };
       }
-      $hourInput.bind("keyup", onKeyUp($minuteInput));
-      $minuteInput.bind("keyup", onKeyUp($secondsInput));
+      $hoursInput.on("keyup", onKeyUp($minutesInput));
+      $minutesInput.on("keyup", onKeyUp($secondsInput));
       function onBlurInput() {
         element.removeClass("focused");
         this.value = leadingZero(Number(this.value));
         updateModel();
       }
-      $hourInput.on("blur", onBlurInput.bind($hourInput.get(0)));
-      $minuteInput.on("blur", onBlurInput.bind($minuteInput.get(0)));
+      $hoursInput.on("blur", onBlurInput.bind($hoursInput.get(0)));
+      $minutesInput.on("blur", onBlurInput.bind($minutesInput.get(0)));
       $secondsInput.on("blur", onBlurInput.bind($secondsInput.get(0)));
     }
   };
--- src/time-input.js
+++ src/time-input.js
@@ -9,22 +9,13 @@ angular.module('time-input', []).directive('timeInput', [function() {
 		restrict: 'C',
 		require: 'ngModel',
 		controller: ['$scope', function ($scope) {
-			$scope.timeObject = {
-				hours : 0,
-				minutes : 0,
-				seconds : 0,
-				lastValidHours: 0,
-				lastValidMinutes: 0,
-				lastValidSeconds: 0
-			};
+			//
 		}],
 		link: function (scope, element, attrs, ngModel) {
 			var $inputs = element.find('input'),
-				$hourInput = $inputs.eq(0),
-				$minuteInput = $inputs.eq(1),
-				$secondsInput = $inputs.eq(2),
-				mousewheel = false,
-				timeout;
+				$hoursInput = $inputs.eq(0),
+				$minutesInput = $inputs.eq(1),
+				$secondsInput = $inputs.eq(2);
 
 			function leadingZero(value) {
 				if (value < 10) {
@@ -34,22 +25,17 @@ angular.module('time-input', []).directive('timeInput', [function() {
 				}
 			}
 
-			$('input', element).on('click',function() {
+			$inputs.on('click',function() {
 				this.select();
 			});
-
-			$('input', element).on('focus',function(){
+			$inputs.on('focus',function(){
 				element.addClass('focused');
 			});
-			$('input', element).on('mousewheel',function(){
-				mousewheel = true;
-				clearTimeout(timeout);
-				timeout = setTimeout(function () {
-					mousewheel = false;
-				},500);
+			$inputs.on('mousewheel',function(){
+				//enables value increasing/decreasing via mouse wheel
 			});
 
-			$('input', element).bind('keydown', function (event) {
+			$inputs.on('keydown', function (event) {
 				var key = event.keyCode;
 				//if  0-9 also or 0-9(numpad) or backspace or delete or arrows
 				if (!(( key >= 48 && key <= 57) || (key >= 96 && key <= 105) || key === 8 || key === 46 || (key >= 37 && key <= 40))) {
@@ -57,90 +43,73 @@ angular.module('time-input', []).directive('timeInput', [function() {
 				}
 			});
 
-			$('input', element).bind('paste cut', function(event) {
+			$inputs.on('paste cut', function(event) {
 				event.preventDefault();
 			});
 
 			ngModel.$render = function render() {
 				var date = ngModel.$viewValue;
 
-				scope.timeObject.hours = date.getHours();
-				scope.timeObject.minutes = date.getMinutes();
-				scope.timeObject.seconds = date.getSeconds();
-
-				scope.timeObject.lastValidHours =  scope.timeObject.hours;
-				scope.timeObject.lastValidMinutes = scope.timeObject.minutes;
-				scope.timeObject.lastValidSeconds = scope.timeObject.seconds;
-
-				$hourInput.val(leadingZero(scope.timeObject.hours));
-				$minuteInput.val(leadingZero(scope.timeObject.minutes));
-				$secondsInput.val(leadingZero(scope.timeObject.seconds));
+				$hoursInput.val(leadingZero(date.getHours()));
+				$minutesInput.val(leadingZero(date.getMinutes()));
+				$secondsInput.val(leadingZero(date.getSeconds()));
 			};
 
 			scope.$watch(attrs.disable, function (newVal) {
 				if(newVal) {
-					$hourInput.attr('disabled','disabled');
-					$minuteInput.attr('disabled','disabled');
+					$hoursInput.attr('disabled','disabled');
+					$minutesInput.attr('disabled','disabled');
 					$secondsInput.attr('disabled','disabled');
 				}else{
-					$hourInput.removeAttr('disabled');
-					$minuteInput.removeAttr('disabled');
+					$hoursInput.removeAttr('disabled');
+					$minutesInput.removeAttr('disabled');
 					$secondsInput.removeAttr('disabled');
 				}
 			});
 
 			function shouldUpdateOnChange() {
-				return !(ngModel.$options &&
-				ngModel.$options.updateOn &&
-				ngModel.$options.updateOn.indexOf('default') === -1);
-
+				return ngModel.$options && ngModel.$options.updateOnChange === true;
 			}
 
 			function updateModel() {
-				var date = ngModel.$viewValue;
-				date.setSeconds(scope.timeObject.seconds);
-				date.setMinutes(scope.timeObject.minutes);
-				date.setHours(scope.timeObject.hours);
-				ngModel.$setViewValue(date);
+				ngModel.$viewValue.setHours(parseInt($hoursInput.val(), 10));
+				ngModel.$viewValue.setMinutes(parseInt($minutesInput.val(), 10));
+				ngModel.$viewValue.setSeconds(parseInt($secondsInput.val(), 10));
+				ngModel.$setViewValue(ngModel.$viewValue);
 				ngModel.$commitViewValue();
 			}
 
 			function inputChange(elem, varName) {
 				return function inputChangeClosure() {
-					if(!elem.value || isNaN(elem.value)){
-						scope.timeObject[varName] = 0;
-						scope.timeObject['lastValid' + varName] = 0;
+					if(!elem.value || isNaN(elem.value)) {
 						elem.value = '00';
 						updateModel();
 						$(elem).select();
 						return;
 					}
 
-					if(elem.value.length > 2){
-						elem.value = leadingZero(scope.timeObject['lastValid' + varName]);
+					if(elem.value.length > 2) {
+						switch(varName) {
+							case 'hours': elem.value = leadingZero(ngModel.$viewValue.getHours()); break;
+							case 'minutes': elem.value = leadingZero(ngModel.$viewValue.getMinutes()); break;
+							case 'seconds': elem.value = leadingZero(ngModel.$viewValue.getSeconds()); break;
+						}
 						return;
 					}
-					if(Number(elem.value)> Number(elem.getAttribute('max'))) {
+
+					if(Number(elem.value) > Number(elem.getAttribute('max'))) {
 						elem.value = elem.value[0];
 					}
-					scope.timeObject[varName] = Number(elem.value);
-					if ((elem.value.length === 2 || elem.value > Number(elem.getAttribute('max')[0])) && !mousewheel) {
-						elem.value = leadingZero(scope.timeObject[varName]);
-					}
-					if(mousewheel) {
-						elem.value = leadingZero(scope.timeObject[varName]);
-					}
-					scope.timeObject['lastValid' + varName] = scope.timeObject[varName];
+
 					if (shouldUpdateOnChange()) {
 						updateModel();
 					}
-
 				};
 			}
 
-			$hourInput.bind('input', inputChange($hourInput.get(0), 'hours'));
-			$minuteInput.bind('input', inputChange($minuteInput.get(0), 'minutes'));
-			$secondsInput.bind('input', inputChange($secondsInput.get(0), 'seconds'));
+			$hoursInput.on('input', inputChange($hoursInput.get(0), 'hours'));
+			$minutesInput.on('input', inputChange($minutesInput.get(0), 'minutes'));
+			$secondsInput.on('input', inputChange($secondsInput.get(0), 'seconds'));
 
 			function onKeyUp($nextInput) {
 				return function (event) {
@@ -154,16 +123,16 @@ angular.module('time-input', []).directive('timeInput', [function() {
 					}
 				}
 			}
-			$hourInput.bind('keyup', onKeyUp($minuteInput));
-			$minuteInput.bind('keyup', onKeyUp($secondsInput));
+			$hoursInput.on('keyup', onKeyUp($minutesInput));
+			$minutesInput.on('keyup', onKeyUp($secondsInput));
 
 			function onBlurInput() {
 				element.removeClass('focused');
 				this.value = leadingZero(Number(this.value));
 				updateModel();
 			}
-			$hourInput.on('blur', onBlurInput.bind($hourInput.get(0)));
-			$minuteInput.on('blur', onBlurInput.bind($minuteInput.get(0)));
+			$hoursInput.on('blur', onBlurInput.bind($hoursInput.get(0)));
+			$minutesInput.on('blur', onBlurInput.bind($minutesInput.get(0)));
 			$secondsInput.on('blur', onBlurInput.bind($secondsInput.get(0)));
 		}
 	};
